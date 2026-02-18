f5ecea10e44898d184715687e4c8c1882b3574e7
Lt8x <lt8bw7@gmail.com>
2026-02-19 00:36:57 +0200

    Create page.tsx

---
+++ app/advertiser/page.tsx
@@ -0,0 +1,7 @@
+export default function Advertiser() {
+  return (
+    <div className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-3xl text-blue-400 font-bold">Advertiser Dashboard</h1>
+    </div>
+  );
+}
