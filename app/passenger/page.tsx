a17e3c1439a14d74b7ff0ba34aca314ec9430136
Lt8x <lt8bw7@gmail.com>
2026-02-19 08:35:57 +0200

    Delete app/passenger directory

--- app/passenger/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Passenger() {
-  return (
-    <div className="min-h-screen bg-black text-white p-10">
-      <h1 className="text-3xl text-blue-400 font-bold">Admin Dashboard</h1>
-    </div>
-  );
-}
