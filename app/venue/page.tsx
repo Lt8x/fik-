e532379c86bacb2b48c2cf92960f1ba967e1aa4d
Lt8x <lt8bw7@gmail.com>
2026-02-19 08:04:26 +0200

    Create page.tsx

---
+++ app/venue/page.tsx
@@ -0,0 +1,7 @@
+export default function Venue() {
+  return (
+    <div className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-3xl text-blue-400 font-bold">Venue Dashboard</h1>
+    </div>
+  );
+}
